d917312386cee20c6c807e618f6e6674a19cbcbf
Tian Dutoit <tiandutoit.nz@gmail.com>
2018-01-25 14:40:50 +1300

    fixed seed file added return

--- seeds/users.js
+++ seeds/users.js
@@ -1,7 +1,7 @@
 exports.seed = function (knex, Promise) {
   return knex('users').del()
     .then(function () {
-      knex('users').insert([{id: 99901, name: 'Ambitious Aardvark', email: 'aardvark@example.org'},
+      return knex('users').insert([{id: 99901, name: 'Ambitious Aardvark', email: 'aardvark@example.org'},
         {id: 99902, name: 'Bamboozled Baboon', email: 'baboon@example.org'},
         {id: 99903, name: 'Curious Capybara', email: 'capybara@example.org'},
         {id: 99904, name: 'Dilapidated Duck', email: 'duck@example.org'},
